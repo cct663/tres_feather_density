49ad4c72ce79d0e99d427dcd545ed4b75d9bedd9
cct663 <cct663@gmail.com>
2020-09-22 15:37:45 -0400

    organizing files

---
+++ 3_r_scripts/1_main_script.R
@@ -0,0 +1,11 @@
+# Analysis of nestling tree swallow feather quality in 2018 & 2019
+# Written by Conor Taff
+# Last updated 9/14/2020
+# Run under R Studio 1.1.463 on Mac OSX 10.11.6
+
+
+# Load packages ----
+  pacman::p_load(tidyverse, here, lme4, scales)
+
+# Load data ----
+  
